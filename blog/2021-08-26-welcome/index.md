c7a35d2a290d7fefe2b407f88b1c6b9b107f02f2
Metin Karakus <mkarakus@vitrolife.com>
2026-01-28 02:03:25 +0300

    translate to en

--- blog/2021-08-26-welcome/index.md
+++ blog/2021-08-26-welcome/index.md
@@ -1,7 +1,7 @@
 ---
 slug: welcome
 title: Welcome
-authors: [slorber, yangshun]
+authors: [metin]
 tags: [facebook, hello, docusaurus]
 ---
 
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,17 +5,20 @@ import styles from './SocialLinks.module.css';
 export default function SocialLinks() {
   return (
     <div className={styles.links}>
+      <Link className={styles.button} href="https://www.youtube.com/@metin-karakus">
+        YouTube
+      </Link>
       <Link className={styles.button} href="https://github.com/m-karakus">
         GitHub
       </Link>
       <Link className={styles.button} href="https://www.linkedin.com/in/metin-karakus-b586b6132">
         LinkedIn
       </Link>
-      <Link className={styles.button} href="https://www.youtube.com/@metin-karakus">
-        YouTube
-      </Link>
       <Link className={styles.button} href="https://www.dropbox.com/scl/fi/979tf7f3ipfjc56sg7q0n/metin_karakus_cv.pdf?rlkey=fr4ix6f8htqopxkbtf3ps4259&st=wvyxd4mc&dl">
         CV (PDF)
+      </Link>
+      <Link className={styles.button} href="mailto:metin_karakus@yahoo.com">
+        Email
       </Link>
     </div>
   );
